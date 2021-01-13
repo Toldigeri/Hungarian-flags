612d4e2699e439dc675066a9957ecaa64344051d
Toldi Gergo <toldigeri57@gmail.com>
2021-01-13 21:23:26 +0100

    Final touches

---
+++ Toldi-Gergo.js
@@ -0,0 +1,58 @@
+function zaszlo(){
+   let szam = document.getElementById('lista').value;
+   let k = document.getElementById('kep');
+
+   switch(+szam){
+      case 0:{
+         alert("Kérlek az alábbi opciók közül válassz");
+      }
+      case 1:{
+         k.src="Kepek/zaszlo" +szam+".png";
+         break;
+      }
+      case 2:{
+         k.src="Kepek/zaszlo" +szam+".png";
+         break;
+      }
+      case 3:{
+         k.src="Kepek/zaszlo" +szam+".png";
+         break;
+      }
+      case 4:{
+         k.src="Kepek/zaszlo" +szam+".png";
+         break;
+      }
+      case 5:{
+         k.src="Kepek/zaszlo" +szam+".png";
+         break;
+      }
+      case 6:{
+         k.src="Kepek/zaszlo" +szam+".png";
+         break;
+      }
+      case 7:{
+         k.src="Kepek/zaszlo" +szam+".png";
+         break;
+      }
+      case 8:{
+         k.src="Kepek/zaszlo" +szam+".png";
+         break;
+      }
+      case 9:{
+         k.src="Kepek/zaszlo" +szam+".png";
+         break;
+      }
+      case 10:{
+         k.src="Kepek/zaszlo" +szam+".png";
+         break;
+      }
+      case 11:{
+         k.src="Kepek/zaszlo" +szam+".png";
+         break;
+      }
+      case 12:{
+         k.src="Kepek/zaszlo" +szam+".png";
+         break;
+      }
+   }
+}
